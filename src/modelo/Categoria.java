50dc36bbf6b2cb5c9d0a5fa4317d56a9322515dd
Glenelly <glenellykarlatbm@gmail.com>
2023-09-11 03:56:23 -0300

    modelo e o DAO de categorias

---
+++ src/modelo/Categoria.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class Categoria {
+
+    private final Integer id;
+    private final String nome;
+
+    public Categoria(Integer id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+}
